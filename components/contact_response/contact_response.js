a5d76309937f2c7b0ee9328de2cc319b0764cfe6
Ghiyas Ud Din Ahmed <ghiyasuddinahmed@gmail.com>
2022-01-02 17:46:19 +0500

    font added in main app

--- components/contact_response/contact_response.js
+++ components/contact_response/contact_response.js
@@ -1,20 +1,12 @@
-import { View } from "react-native";
-import { Image } from "react-native";
-import { Button, Text, Card } from "react-native-paper";
-import logo from "../../assets/logo-transparent.png";
-import { useFonts } from "expo-font";
+import { View } from 'react-native';
+import { Image } from 'react-native';
+import { Button, Text, Card } from 'react-native-paper';
+import logo from '../../assets/logo-transparent.png';
 
-import globalStyles from "../../globalStyles";
-import styles from "./styles";
+import globalStyles from '../../globalStyles';
+import styles from './styles';
 
-export default function ContactResponse({}) {
-  const [loaded] = useFonts({
-    LuxuriousRoman: require("../../assets/fonts/LuxuriousRoman-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    return null;
-  }
+export default function ContactResponse({ navigation }) {
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.imageContainer} />
@@ -23,10 +15,10 @@ export default function ContactResponse({}) {
         <Text style={styles.response_text}>We will be in touch shortly!</Text>
       </Card>
       <Button
-        icon="page-next-outline"
-        mode="contained"
-        color={"white"}
-        onPress={() => alert("Hi")}
+        icon='page-next-outline'
+        mode='contained'
+        color={'white'}
+        onPress={() => navigation.goBack()}
         styles={globalStyles.button}
       >
         Continue
